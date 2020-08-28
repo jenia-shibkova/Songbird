e431b7d9d15ecb3857cf3b69e19f944088913a54
Eugenia Shibkova <shibkova91@gmail.com>
2020-08-28 21:10:33 +0300

    refactor: refactor header module

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,34 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Header.scss';
 
-const Header = ({ score }) => {
-  return (
-    <header className="header d-flex">
-      <div className="top-panel d-flex">
-        <h1>Songbird</h1>
-        <h5 className="score">score: {score} </h5>
-      </div>
+export default class Header extends Component {
 
-      <ul className="pagination">
-        <li className="page-item active">
-          <a className="page-link" href="/#">Разминка</a>
-        </li>
-        <li className="page-item">
-          <a className="page-link" href="/#">Воробьиные</a>
-        </li>
-        <li className="page-item">
-          <a className="page-link" href="/#">Лесные птицы</a>
-        </li><li className="page-item">
-          <a className="page-link" href="/#">Певчие птицы</a>
-        </li><li className="page-item">
-          <a className="page-link" href="/#">Хищные птицы</a>
-        </li>
-        <li className="page-item">
-          <a className="page-link" href="/#">Морские птицы</a>
-        </li>
-      </ul>           
-    </header>
-  );
-};
+  state = {
+    navItems: ['Разминка', 'Воробьиные', 'Лесные птицы', 'Певчие птицы', 'Хищные птицы', 'Морские птицы']
+  }
 
-export default Header;
+  getElements = (page) => {
+    const { navItems } = this.state;
+
+    return navItems.map((item, index) => {    
+      let className = 'page-item';
+  
+      if (index === page) {
+        className += ' active';
+      }
+  
+      return (
+        <li key={index} className={className}>
+          <a className="page-link" href="/#">{item}</a>
+        </li>
+      )
+    });
+  };
+
+  render() {
+    const { score, page } = this.props;
+
+    return (
+      <header className="header d-flex">
+        <div className="top-panel d-flex">
+          <h1>Songbird</h1>
+          <h5 className="score">score: {score} </h5>
+        </div>
+  
+        <ul className="pagination">
+          { this.getElements(page) }
+        </ul>           
+      </header>
+    )
+  } 
+}
