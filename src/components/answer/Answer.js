a6308445ca4faecb9e2088616ab34e715d3063b9
Eugenia Shibkova <shibkova91@gmail.com>
2020-08-30 23:33:49 +0300

    refactor: refactor Answer module structure

--- src/components/answer/Answer.js
+++ src/components/answer/Answer.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import './Answer.scss';
+import AudioPlayer from '../audio-player';
 
-export default class Answer extends Component {
-
-  render() {
-    return (
+function Answer(props) {
+  console.log(props)
+  return (
       <div className="bird-details card">
         <p className="instruction">
           <span>Послушайте плеер.</span>
@@ -20,22 +21,7 @@ export default class Answer extends Component {
               <span>Corvus corax</span>
             </li>
             <li className="list-group-item">
-              <div className="audio-player">
-                <audio src="https://www.xeno-canto.org/sounds/uploaded/XIQVMQVUPP/XC518684-Grands%20corbeaux%2009012020%20Suzon.mp3"></audio>
-                <div className="controls">
-                  <div className="playback-button">
-                    <svg viewBox="-200 0 1200 1000"><path fill="#00bc8c" d="M96.51 11.97c-31.23 8.05-53.26 32.76-63.42 71.27-3.45 12.84-3.64 29.7-3.64 416.71s.19 403.87 3.64 416.71c16.09 60.74 61.69 86.03 120.9 67.25 9-2.87 53.65-25.1 116.49-58.24 56.14-29.51 221.29-116.3 367.28-192.93 145.99-76.64 271.29-143.31 278.38-148.1 39.28-25.68 59.59-63.04 53.26-97.52-4.79-26.63-24.33-53.07-52.88-71.65C892 399.37 172.58 22.32 154.95 16.38c-18.97-6.33-43.3-8.24-58.44-4.41z"></path></svg>
-                  </div>
-                  <div className="timebar ">
-                    <div className="timebar-bar" ></div>
-                    <div className="timebar-circle"></div>
-                    <div className="timebar-time-info">
-                      <div>00:06</div>
-                      <div>00:12</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <AudioPlayer />
             </li>
           </ul>
         </div>
@@ -43,6 +29,13 @@ export default class Answer extends Component {
           Ворон – крупная птица. Длина тела достигает 70 сантиметров, размах крыльев – до полутора метров. Вороны населяют окрестности Тауэра. В Англии бытует поверье, что в день, когда черные вороны улетят от Тауэра, монархия рухнет.
         </p>
       </div>
-    )
-  }
+  )
 }  
+
+const mapStateToProps = (state) => {
+  return {
+    chosenBird: state.chosenBird.chosenBird,
+  }
+}
+
+export default connect(mapStateToProps, null)(Answer);
